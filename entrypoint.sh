9529530a7d4ec305cd906e4a295674f98b4b20e1
Mirko Schaefer <mirko.schaefer@sap.com>
2024-07-10 11:00:51 +0200

    fix: move to pg_dump

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,9 @@ then
     echo "$(get_date) !WARNING! It's strongly recommended to encrypt your backups."
 fi
 
-echo "$(get_date) Postgres backup started"
+echo "$(get_date) Postgres backup started of DB $DATABASE from $DATABASE_HOST with port $DATABASE_PORT and user $DATABASE_USER"
 
-PGPASSWORD=$DATABASE_PASSWORD pg_dump -c -U $DATABASE_USER -h $DATABASE_HOST -p $DATABASE_PORT -O -v $DATABASE | aws s3 cp - s3://$AWS_BUCKET/$FILE_PREFIX`date +%d-%m-%Y`.sql
+aws configure set default.s3.multipart_chunksize 200MB 
+PGPASSWORD=$DATABASE_PASSWORD pg_dump -c -U $DATABASE_USER -h $DATABASE_HOST -p $DATABASE_PORT --no-owner --no-privileges -v $DATABASE | aws s3 cp - s3://$AWS_BUCKET/$FILE_PREFIX`date +%d-%m-%Y`.sql
 
 echo "$(get_date) Postgres backup completed successfully!"
